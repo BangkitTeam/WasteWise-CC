d0d8f44e4df26955045a2cd2ba12e680e8dc42ce
Andrea <andrea.marie@ti.ukdw.ac.id>
2024-12-03 04:22:58 +0700

    tambah api buat beberapa kolom tabel

--- src/craft/craftController.js
+++ src/craft/craftController.js
@@ -1,19 +1,47 @@
 const { Router } = require("express");
+const multer = require("multer");
 const { addCraft, getCrafts, deleteCraft } = require("./craftService");
 const { validateRequest } = require("../middlewares/validationMiddleware");
 const { createCraftSchema } = require("./craftValidations");
 
 const router = Router();
 
+// Konfigurasi multer untuk mengelola upload file
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./craftImage"); // Folder tempat menyimpan file
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`); // Penamaan file
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpg", "image/jpeg", "image/png"];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type. Only JPG, JPEG, and PNG are allowed."));
+    }
+  },
+});
+
 // Route untuk menambah Craft
 router.post(
   "/addcrafts",
-  validateRequest(createCraftSchema), // Validasi input untuk pembuatan craft
+  upload.single("imageUrl"),
+  validateRequest(createCraftSchema),
   async (req, res) => {
-    const { title, description } = req.body;
-
+    const { title, description, wasteType, tutorialUrl } = req.body; // Menangkap tutorialUrl
+    const imageUrl = req.file ? `/craftImage/${req.file.filename}` : null;
     try {
-      const addedCraft = await addCraft(title, description);
+      const addedCraft = await addCraft(
+        title,
+        description,
+        wasteType,
+        imageUrl,
+        tutorialUrl // Menambahkan tutorialUrl
+      );
       res.status(201).json({ message: "Craft added successfully", addedCraft });
     } catch (error) {
       res.status(400).json({ error: error.message });
@@ -31,13 +59,12 @@ router.get("/crafts", async (req, res) => {
   }
 });
 
-// Delete craft ini perlu atau tidak ya ?
+// Route untuk menghapus Craft
 router.delete("/crafts/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const response = await deleteCraft(Number(id));
-    // Menambahkan ID craft dalam pesan sukses
     res
       .status(200)
       .json({ message: `Craft ID ${id} deleted successfully`, response });
@@ -47,4 +74,3 @@ router.delete("/crafts/:id", async (req, res) => {
 });
 
 module.exports = router;
-
